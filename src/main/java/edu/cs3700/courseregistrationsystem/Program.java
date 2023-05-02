36a3955804808f712b1f2480e98c2310546f9d8b
jfond <jfond@LAPTOP-HREA6J2S.wireless.indianatech.net>
2023-05-02 13:57:53 -0400

    added program class

--- src/main/java/edu/cs3700/courseregistrationsystem/Program.java
+++ src/main/java/edu/cs3700/courseregistrationsystem/Program.java
@@ -6,8 +6,36 @@ package edu.cs3700.courseregistrationsystem;
 
 /**
  *
- * @author Broxt
+ * @author Jon
  */
 public class Program {
-    
-}
+        public String Name;
+        public String College;
+        private String Type;
+        private int Num_credits;
+        
+        public String getType(){
+            return(Type);
+        }
+        
+        public int getNum_credits(){
+            return(Num_credits);
+        }
+        
+        public int setType(String Type){
+            if (Type.equalsIgnoreCase("traditional")|| 
+                    Type.equalsIgnoreCase("online")){
+                this.Type = Type;
+                return 0;
+            }
+            return 1;
+        }
+        
+        public int setNum_credits(int Num_credits){
+            if (Num_credits > 0){
+                this.Num_credits = Num_credits;
+                return 0;
+            }
+            return 1;
+        }
+    }
