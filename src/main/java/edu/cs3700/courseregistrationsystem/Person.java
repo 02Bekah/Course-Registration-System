bbb798554d47bd5634b517f359e723ba1896f3dd
BroxtonButler <broxtonbutler@outlook.com>
2023-05-02 12:48:23 -0400

    Added empty Program class. Added Person class

---
+++ src/main/java/edu/cs3700/courseregistrationsystem/Person.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.cs3700.courseregistrationsystem;
+
+/**
+ *
+ * @author Broxt
+ */
+public class Person {
+    public String Name;
+    public Date DateOfBirth;
+    public Program CurrentProgram;
+    public Course[] CourseList;
+    
+    public Person(){
+        
+    }
+    
+    public Person(String name, Date dob, Program currentProgram, Course[] courseList){
+        Name = name;
+        DateOfBirth = dob;
+        CurrentProgram = currentProgram;
+        CourseList = courseList;
+    }
+    
+    
+}
