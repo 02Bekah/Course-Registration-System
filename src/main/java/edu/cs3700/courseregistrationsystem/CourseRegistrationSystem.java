2873907fe93f027d5460ae2bd3919250c8bb6faa
Rebekah <RKAlbrecht01@indianatech.net>
2023-05-04 13:47:17 -0400

    added instructors[] array and populateInstructor() method in CourseRegistrationSystem

--- src/main/java/edu/cs3700/courseregistrationsystem/CourseRegistrationSystem.java
+++ src/main/java/edu/cs3700/courseregistrationsystem/CourseRegistrationSystem.java
@@ -8,11 +8,20 @@ import javax.swing.JFrame;
  */
 
 public class CourseRegistrationSystem {
+    // Instructor array
+    public static Instructor[] instructors = new Instructor[100];
 
     public static void main(String[] args) {
         MainWindow mWindow = new MainWindow();
         mWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mWindow.setSize(900, 900);
         mWindow.setVisible(true);
+    }
+    
+    public void populateInstructor() {
+        instructors[0] = new Instructor("Jackson Blakely", new Date(05,1,1990));
+        instructors[1] = new Instructor("Clementine Blakely", new Date(22,2,1992));
+        instructors[2] = new Instructor("Joshua Clark", new Date(18,9,1950));
+        instructors[3] = new Instructor("Ashley Miller", new Date(30,12,1969));
     }
 }
