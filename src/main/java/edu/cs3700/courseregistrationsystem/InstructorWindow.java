50250e9bd11f73c2a39cc2f2676420ba06b86bf4
Rebekah <RKAlbrecht01@indianatech.net>
2023-05-11 12:34:21 -0400

    formatting

--- src/main/java/edu/cs3700/courseregistrationsystem/InstructorWindow.java
+++ src/main/java/edu/cs3700/courseregistrationsystem/InstructorWindow.java
@@ -56,47 +56,48 @@ public class InstructorWindow extends JFrame{
         
         
         // Initialize title labels
-        instructorNameTitleLbl = new JLabel("Name");
+        /*
+        instructorNameTitleLbl = new JLabel("Name                    ");
         instructorNameTitleLbl.setFont(instructorTitleFont);
-        instructorDOBTitleLbl = new JLabel("Date of Birth:");
+        instructorDOBTitleLbl = new JLabel("Date of Birth:          ");
         instructorDOBTitleLbl.setFont(instructorTitleFont);
-        instrutorProgramTitleLbl = new JLabel("Programs:");
+        instrutorProgramTitleLbl = new JLabel("Programs:            ");
         instrutorProgramTitleLbl.setFont(instructorTitleFont);
-        instrutorCourseTitleLbl = new JLabel("Courses:");
+        instrutorCourseTitleLbl = new JLabel("Courses:              ");
         instrutorCourseTitleLbl.setFont(instructorTitleFont);
         instrutorAdviseeTitleLbl = new JLabel("Advisees");
         instrutorAdviseeTitleLbl.setFont(instructorTitleFont);
-        
         
         add(instructorNameTitleLbl);
         add(instructorDOBTitleLbl);
         add(instrutorProgramTitleLbl);
         add(instrutorCourseTitleLbl);
         add(instrutorAdviseeTitleLbl);
+        */
         
         for (int i=0; i<searchResults.length; i++) {
             // Initialize instructor name label
-            instructorNameLbls[i] = new JLabel(searchResults[i].Name);
+            instructorNameLbls[i] = new JLabel("Name: " + searchResults[i].Name);
             instructorNameLbls[i].setFont(instructorInfoFont);
             add(instructorNameLbls[i]);
             
             // Initialize instructor date of birth label
-            instructorDOBLbls[i] = new JLabel(searchResults[i].DateOfBirth.toString());
+            instructorDOBLbls[i] = new JLabel("DOB: " +searchResults[i].DateOfBirth.toString());
             instructorDOBLbls[i].setFont(instructorInfoFont);
             add(instructorDOBLbls[i]);
             
             // Initialize instructor program label
-            instructorProgramLbls[i] = new JLabel(searchResults[i].CurrentProgram.Name);
+            instructorProgramLbls[i] = new JLabel("Program: " +searchResults[i].CurrentProgram.Name);
             instructorProgramLbls[i].setFont(instructorInfoFont);
             add(instructorProgramLbls[i]);
             
             // Initialize instructor courses label
-            instructorCoursesLbls[i] = new JLabel(searchResults[i].instructorCoursesToString());
+            instructorCoursesLbls[i] = new JLabel("Courses: " +searchResults[i].instructorCoursesToString());
             instructorCoursesLbls[i].setFont(instructorInfoFont);
             add(instructorCoursesLbls[i]);
             
             // Initialize intrsuctor advisees label
-            instructorAdviseesLbls[i] = new JLabel(searchResults[i].instructorAdviseesToString());
+            instructorAdviseesLbls[i] = new JLabel("Advisees: " + searchResults[i].instructorAdviseesToString());
             instructorAdviseesLbls[i].setFont(instructorInfoFont);
             add(instructorAdviseesLbls[i]);
         }
